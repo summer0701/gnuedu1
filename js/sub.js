3d7f95e18aae38ea85317fccb007a53cd5a927f9
summer0701 <garts22@gmail.com>
2017-11-09 18:04:57 +0900

    PageControl 추가

--- js/sub.js
+++ js/sub.js
@@ -19,8 +19,7 @@
 			load(SearchTextJS.value);
 
 		}
-
-		function load(keyword){
+		function loadNaver(keyword){
 			
 			var status = 0;
 			var response_ok = false;
@@ -58,10 +57,67 @@
  
 	  
 			}
+		function load(keyword){
+			
+			var status = 0;
+			var response_ok = false;
+			
+
+			var urlsub = "https%3A%2F%2Fnews.google.com%2Fnews%2Frss%2Fheadlines%2Fsection%2Ftopic%2FSPORTS.ko_kr%2F%EC%8A%A4%ED%8F%AC%EC%B8%A0%3Fned%3Dkr%26hl%3Dko";
+			if(keyword =="스포츠") {
+				urlsub = "https%3A%2F%2Fnews.google.com%2Fnews%2Frss%2Fheadlines%2Fsection%2Ftopic%2FSPORTS.ko_kr%2F%EC%8A%A4%ED%8F%AC%EC%B8%A0%3Fned%3Dkr%26hl%3Dko";
+			}else if(keyword =="경제") {
+				urlsub = "https%3A%2F%2Fnews.google.com%2Fnews%2Fu%2F1%2Frss%2Fheadlines%2Fsection%2Ftopic%2FBUSINESS.ko_kr%2F%25EA%25B2%25BD%25EC%25A0%259C%3Fned%3Dkr%26amp%3Bauthuser%3D1%26hl%3Dko%26gl%3DKR";
+			}else if(keyword =="문화") {
+				urlsub = "https%3A%2F%2Fnews.google.com%2Fnews%2Fu%2F1%2Frss%2Fheadlines%2Fsection%2Ftopic%2FENTERTAINMENT.ko_kr%2F%25EC%2597%25B0%25EC%2598%2588%3Fned%3Dkr%26amp%3Bauthuser%3D1%26hl%3Dko%26gl%3DKR";
+			}else if(keyword =="IT") {
+				urlsub = "https%3A%2F%2Fnews.google.com%2Fnews%2Fu%2F1%2Frss%2Fheadlines%2Fsection%2Ftopic%2FSCITECH.ko_kr%2F%25EA%25B3%25BC%25ED%2595%2599%25EA%25B8%25B0%25EC%2588%25A0%3Fned%3Dkr%26amp%3Bauthuser%3D1%26hl%3Dko%26gl%3DKR";
+			}else{
+				loadNaver(keyword);
+				return;
+
+			}
+			
+
+			var url = "https://api.rss2json.com/v1/api.json?rss_url="+urlsub+"&api_key=a6thfhv2ngviiop4zksqugkedhfqfke3hbtqyo5h&_=1510204745681";
+			console.log(url);
+			fetch(url, {
+				method: 'GET',
+			
+				
+			     
+			}).then(function(response) {
+			    status = response.status;  // Get the HTTP status code
+			    response_ok = response.ok; // Is response.status in the 200-range?
+			  
+			    return response.json();    // This returns a promise
+			}).then(function(responseObject) {
+				//console.log(e.value);
+				//console.log(JSON.stringify(responseObject));
+				var regExString = /(<([^>]+)>)/ig;
+				e.clear();
+				for(var i=0; i<responseObject.items.length;i++) {
+					//contentString = responseObject.items[i].replace(regExString, "");
+					responseObject.items[i].description = responseObject.items[i].description.replace(regExString, "");
+					console.log(JSON.stringify(responseObject.items[i].description));
+					e.add(responseObject.items[i]); 	
+				}
+				//e.remove(1);
+				//console.log(JSON.stringify(e.value)); 
+
+				
+			    // Do something with the result
+			}).catch(function(err) {
+				
+			    // An error occurred somewhere in the Promise chain
+			});
+ 
+	  
+			}
 		function OnEventCardClick(arg)
 		{
-			console.log(JSON.stringify(arg.data.originallink));
-			InterApp.launchUri(arg.data.originallink);	
+			console.log(JSON.stringify(arg.data.link));
+			InterApp.launchUri(arg.data.link);	
 		}			
 		module.exports = {
 			gomain: function() { router.goBack();},
